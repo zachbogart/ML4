a5fb9acd6007dcd47ccb3484d2206e881be09107
zachbogart <zlbogart@gmail.com>
2018-09-16 05:03:42 -0400

    update travis creds

--- _deploy.sh
+++ _deploy.sh
@@ -5,7 +5,7 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "zb2223@columbia.edu"
+git config --global user.email "zlbogart@gmail.com"
 git config --global user.name "Zach Bogart"
 
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
